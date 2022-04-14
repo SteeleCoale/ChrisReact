5bedb6fd3bac4a68e8f5643e115aab4e2b9c0060
Logan Coale <logancoale@administrators-MacBook-Pro.local>
2022-04-14 16:34:45 -0400

    an app with a component!

---
+++ chris_app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { FirstComponent } from './FirstComponent';
+
+function App() {
+  return (
+    <>
+   <p>Hi Chris</p>
+   <FirstComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ chris_app/src/FirstComponent.js
@@ -0,0 +1,11 @@
+
+/** Functional Components in React look a lot like normal JS functions... */
+
+export const FirstComponent = () => {
+
+  return (
+    <div className='firstComponent'>
+      This is the in the First Component
+    </div>
+  )
+}
