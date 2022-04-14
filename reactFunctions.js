df8ff5d27a73c5914a1f93155f4918e9e5eb9277
Logan Coale <logancoale@administrators-MacBook-Pro.local>
2022-04-14 16:10:25 -0400

    functions

---
+++ reactFunctions.js
@@ -0,0 +1,12 @@
+/** This is how normal funcitons can look */
+
+function addTheseThings (item1, item2) {
+  return item1 + item2;
+};
+console.log(addTheseThings(1,4));
+
+const addOtherThings = (item1, item2) => {
+  return item1 + item2;
+};
+console.log(addOtherThings(5, 4));
+
